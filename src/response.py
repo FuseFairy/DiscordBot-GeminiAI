18c13b18589226f7b37e2dcfbdab298ca304fd86
FuseFairy <dd8611706@gmal.com>
2024-02-16 17:51:19 +0800

    feat: remove webhook

--- src/response.py
+++ src/response.py
@@ -1,11 +1,10 @@
 import discord
-import asyncio
 import google.generativeai as genai
 from .log import setup_logger
 
 logger = setup_logger(__name__)
 
-async def send_message(chatbot: genai.ChatSession, message: str, thread: discord.Thread, avatar_url=None, ch_name = None, webhook: discord.Webhook=None):  
+async def send_message(chatbot: genai.ChatSession, message: str, thread: discord.Thread):  
     try:          
         response = await chatbot.send_message_async(message)
             
@@ -15,16 +14,9 @@ async def send_message(chatbot: genai.ChatSession, message: str, thread: discord
         while len(text) > 2000:
             temp = text[:2000]
             text = text[2000:]
-
-            if webhook:
-                await webhook.send(content=temp, username=ch_name, avatar_url=avatar_url, thread=thread)
-            else:
-                await thread.send(temp)
-
-        if webhook:
-            await webhook.send(content=text, username=ch_name, avatar_url=avatar_url, thread=thread)
-        else:
-            await thread.send(text)
+            await thread.send(temp)
+            
+        await thread.send(text)
     except Exception as e:
         await thread.send(f">>> **ERROR：{e}**")
         logger.error(f"ERROR：{e}")
