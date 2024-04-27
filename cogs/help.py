b46505e766b39d785a4634445ce327ea75a8a953
FuseFairy <dd8611706@gmail.com>
2024-04-28 07:11:32 +0800

    refactor: update command

--- cogs/help.py
+++ cogs/help.py
@@ -2,6 +2,7 @@ import discord
 from core.classes import Cog_Extension
 from discord import app_commands
 from src.check_channel import check_channel
+
 class Help(Cog_Extension):
     @app_commands.command(name = "help", description = "Show how to use")
     async def help(self, interaction: discord.Interaction):
@@ -9,9 +10,10 @@ class Help(Cog_Extension):
         if not await check_channel(interaction, "HELP_CMD_CHANNEL_ID"):
             return
         embed=discord.Embed(description="[FuseFairy/DiscordBot-GeminiAI](https://github.com/FuseFairy/DiscordBot-GeminiAI/blob/main/README.md)\n***COMMANDS -***")
-        embed.add_field(name="/api_key setting", value="Can set or delete your personal api key.", inline=False)
-        embed.add_field(name="/create conversation", value="Create thread for conversation.", inline=False)
-        embed.add_field(name="/reset conversation", value="Reset your conversation.", inline=False)
+        embed.add_field(name="/cookies setting-bard", value="Can set or delete your personal Bard cookies.", inline=False)
+        embed.add_field(name="/bard conversation", value="Create thread for Bard conversation.", inline=False)
+        embed.add_field(name="/api_key setting", value="Can set or delete your personal Gemini api key.", inline=False)
+        embed.add_field(name="/bard conversation", value="Create thread for Gemini conversation.", inline=False)
         await interaction.followup.send(embed=embed)
 
 async def setup(bot):
